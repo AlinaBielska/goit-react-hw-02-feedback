4869ba2fcbc0a5bc5cfec2714eaf8c8fd0300f04
AlinaBielska <alinkabelka5@gmail.com>
2023-05-20 23:14:02 +0200

    krok 2: dodałam funcki do podpiczania total i % pozytywnych odpowiedzi

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import css from './Feedback.module.css';
 
 class Feedback extends Component {
     constructor() {
@@ -8,18 +9,26 @@ class Feedback extends Component {
             good: 0,
             neutral: 0,
             bad: 0
-        }
-    }
+        } 
+    };
 
     changeFeedback = (type) => {
-        this.setState(prevState => ({[type]: prevState[type] + 1}));
+        this.setState(prevState => ({ [type]: prevState[type] + 1 }));
+    };
+
+    countTotalFeedback = () => {
+        return this.state.good + this.state.neutral + this.state.bad;
+    };
+
+    countPositiveFeedbackPercentage = () => {
+        return Math.round(this.state.good * 100 / (this.state.good + this.state.neutral + this.state.bad));
     }
 
     render() {
         return (
             <div>
                 <h1>Please leave feedback</h1>
-                <ul>
+                <ul className={css.buttonsFeedback}>
                     <li><button type="button" onClick={() => this.changeFeedback("good")}>Good</button></li>
                     <li><button type="button" onClick={() => this.changeFeedback("neutral")}>Neutral</button></li>
                     <li><button type="button" onClick={() => this.changeFeedback("bad")}>Bad</button></li>
@@ -29,6 +38,8 @@ class Feedback extends Component {
                     <li>Good: <span>{this.state.good}</span></li>
                     <li>Neutral: <span>{this.state.neutral}</span></li>
                     <li>Bad: <span>{this.state.bad}</span></li>
+                    <li>Total: <span>{ this.countTotalFeedback() }</span></li>
+                    <li>Positive feedback: <span>{ this.countPositiveFeedbackPercentage() }%</span></li>
                 </ul>
             </div>
         )
